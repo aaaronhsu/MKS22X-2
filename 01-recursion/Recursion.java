3fcc4ebed8452baf2ae6eea60668ab7a19e8d808
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-05 14:16:04 -0500

    reverse method

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,3 +1,6 @@
 public class Recursion {
-	
+	public static String reverse(String s) {
+		if (s.length() <= 1) return s;
+		return s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
+	}
 }
