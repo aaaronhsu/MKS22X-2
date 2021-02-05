9fad6efb879d069460330f0ae52aa4499276c774
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-05 14:18:27 -0500

    countwords

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -3,4 +3,24 @@ public class Recursion {
 		if (s.length() <= 1) return s;
 		return s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
 	}
+
+	public static long countNoDoubleLetterWords(int length, String word) {
+		if (length == 0) return 1;
+
+		long ret = 0;
+
+		for (char i = 'a'; i <= 'z'; i++) {
+			if (word.length() == 0 || word.charAt(word.length() - 1) != i) {
+				ret += countNoDoubleLetterWords(length - 1, word + i);
+			}
+		}
+
+		for (char i = 'A'; i <= 'Z'; i++) {
+			if (word.length() == 0 || word.charAt(word.length() - 1) != i) {
+				ret += countNoDoubleLetterWords(length - 1, word + i);
+			}
+		}
+
+		return ret;
+	}
 }
