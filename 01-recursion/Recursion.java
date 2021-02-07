387656d61c3d25540b2b2ec3f71971a5319ffb13
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-07 16:39:59 -0500

    removed the main method

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,9 +1,4 @@
 public class Recursion {
-	public static void main(String[] args) {
-		System.out.println(countNoDoubleLetterWords(3, ""));
-		System.out.println(sqrt(Double.parseDouble(args[0])));
-	}
-
 	public static String reverse(String s) {
 		if (s.length() <= 1) return s;
 		return s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
