af4d998f39c785676e268e241f6fc79d478f9945
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-11 13:12:24 -0500

    toString

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -4,4 +4,19 @@ public class QueenBoard {
 	public QueenBoard(int size) {
 		board = new int[size][size];
 	}
+
+	public String toString() {
+		String ret = "";
+		for (int row = 0; row < board.length; row++) {
+			String hold = "";
+			for (int space = 0; space < board[row].length; space++) {
+				hold += board[row][space] == -1 ? "Q" : "_";
+				if (space != board[row].length - 1) hold += " ";
+				else if (row != board.length - 1) hold += "\n";
+			}
+			ret += hold;
+		}
+		
+		return ret;
+	}
 }
--- 02-queens/QueensTester.java
+++ 02-queens/QueensTester.java
@@ -1,5 +1,7 @@
 public class QueensTester {
 	public static void main(String[] args) {
 		QueenBoard board = new QueenBoard(4);
+
+		System.out.println(board);
 	}
 }
