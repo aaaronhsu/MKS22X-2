c20b03e138411657a384756e5638b155707e7684
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-11 16:59:28 -0500

    modified add and remove queen to change values in board

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -5,12 +5,27 @@ public class QueenBoard {
 		board = new int[size][size];
 	}
 
+	// public String toString() {
+	// 	String ret = "";
+	// 	for (int row = 0; row < board.length; row++) {
+	// 		String hold = "";
+	// 		for (int space = 0; space < board[row].length; space++) {
+	// 			hold += board[row][space] == -1 ? "Q" : "_";
+	// 			if (space != board[row].length - 1) hold += " ";
+	// 			else if (row != board.length - 1) hold += "\n";
+	// 		}
+	// 		ret += hold;
+	// 	}
+		
+	// 	return ret;
+	// }
+
 	public String toString() {
 		String ret = "";
 		for (int row = 0; row < board.length; row++) {
 			String hold = "";
 			for (int space = 0; space < board[row].length; space++) {
-				hold += board[row][space] == -1 ? "Q" : "_";
+				hold += board[row][space];
 				if (space != board[row].length - 1) hold += " ";
 				else if (row != board.length - 1) hold += "\n";
 			}
@@ -22,6 +37,7 @@ public class QueenBoard {
 
 	private boolean addQueen(int r, int c) {
 		if (board[r][c] == 0) {
+			modify(r, c, 1);
 			board[r][c] = -1;
 			return true;
 		}
@@ -29,6 +45,27 @@ public class QueenBoard {
 	}
 
 	private void removeQueen(int r, int c) {
+		modify(r, c, -1);
 		board[r][c] = 0;
 	}
+
+	private void modify(int r, int c, int mod) {
+		for (int i = c; i < board.length; i++) {
+			board[r][i] += mod; // horizontal
+			if (r + i - c < board.length) board[r + i - c][i] += mod; // bottom diag
+			if (r - i + c >= 0) board[r - i + c][i] += mod; // top diag
+		}
+	}
+
+	// public boolean solve() throws IllegalStateException {
+		
+	// }
+
+	// private boolean solve(int col) {
+	// 	if (col == board.length) return true;
+
+	// 	for (int i = 0; i < board.length; i++) {
+	// 		if (addQueen(i, col))
+	// 	}
+	// }
 }
--- 02-queens/QueensTester.java
+++ 02-queens/QueensTester.java
@@ -1,7 +1,21 @@
 public class QueensTester {
 	public static void main(String[] args) {
-		QueenBoard board = new QueenBoard(4);
+		QueenBoard board = new QueenBoard(8);
 
 		System.out.println(board);
+		System.out.println();
+
+		board.addQueen(0, 0);
+
+		System.out.println(board);
+		System.out.println();
+
+		board.addQueen(3, 4);
+		System.out.println(board);
+		System.out.println();
+
+		board.removeQueen(0, 0);
+		System.out.println(board);
+		System.out.println();
 	}
 }
