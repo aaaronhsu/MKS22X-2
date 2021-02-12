e2482e598f38ad0ed8aff358bfa03a811aba2c20
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-11 19:05:09 -0500

    revert toString to Q _

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -5,27 +5,12 @@ public class QueenBoard {
 		board = new int[size][size];
 	}
 
-	// public String toString() {
-	// 	String ret = "";
-	// 	for (int row = 0; row < board.length; row++) {
-	// 		String hold = "";
-	// 		for (int space = 0; space < board[row].length; space++) {
-	// 			hold += board[row][space] == -1 ? "Q" : "_";
-	// 			if (space != board[row].length - 1) hold += " ";
-	// 			else if (row != board.length - 1) hold += "\n";
-	// 		}
-	// 		ret += hold;
-	// 	}
-		
-	// 	return ret;
-	// }
-
 	public String toString() {
 		String ret = "";
 		for (int row = 0; row < board.length; row++) {
 			String hold = "";
 			for (int space = 0; space < board[row].length; space++) {
-				hold += board[row][space];
+				hold += board[row][space] == -1 ? "Q" : "_";
 				if (space != board[row].length - 1) hold += " ";
 				else if (row != board.length - 1) hold += "\n";
 			}
@@ -34,6 +19,21 @@ public class QueenBoard {
 		
 		return ret;
 	}
+
+	// public String toString() {
+	// 	String ret = "";
+	// 	for (int row = 0; row < board.length; row++) {
+	// 		String hold = "";
+	// 		for (int space = 0; space < board[row].length; space++) {
+	// 			hold += board[row][space];
+	// 			if (space != board[row].length - 1) hold += " ";
+	// 			else if (row != board.length - 1) hold += "\n";
+	// 		}
+	// 		ret += hold;
+	// 	}
+		
+	// 	return ret;
+	// }
 
 	private boolean addQueen(int r, int c) {
 		if (board[r][c] == 0) {
