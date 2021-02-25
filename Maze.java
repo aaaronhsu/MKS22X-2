d315924870820f2bfab332edc50fe8c3713298ba
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-25 13:58:30 -0500

    removed backtracking char replaced with @

--- Maze.java
+++ Maze.java
@@ -36,30 +36,19 @@ public class Maze {
         if (board[i][j] == 'S') solve(board, i, j);
       }
     }
-
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[0].length; j++) {
-        if (board[i][j] == '*' || board[i][j] == '.') board[i][j] = ' ';
-      }
-    }
   }
 
   public static boolean solve(char[][] board, int x, int y) {
-    if (board[x][y] == '#' || board[x][y] == '*' || board[x][y] == '.') return false;
+    if (board[x][y] == '#' || board[x][y] == '@' || board[x][y] == '.') return false;
     //        wall                curr search              seen
     if (board[x][y] == 'E') return true;
 
-    board[x][y] = '*';
+    board[x][y] = '@';
 
     boolean solution = solve(board, x + 1, y) || solve(board, x - 1, y) || solve(board, x, y + 1) || solve(board, x, y - 1);
     
-    if (solution) {
-      board[x][y] = '@';
-      return true;
-    }
-
-    board[x][y] = '.';
-    return false;
+    if (!solution) board[x][y] = '.';
+    return solution;
 
   }
 }
