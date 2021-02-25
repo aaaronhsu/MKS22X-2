a8de1bce132a77bad358e1ff4a1778701c96a4e7
Aaron Hsu <pufflewizzy@gmail.com>
2021-02-25 14:20:57 -0500

    copied over given animation functions

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -17,5 +17,24 @@ public class Maze {
     for (int i = 0; i < l.size(); i++) maze[i] = l.get(i);
   }
 
+  private void wait(int millis) {
+    try {
+      Thread.sleep(millis);
+    }
+    catch (InterruptedException e) {}
+  }
+
+  public void setAnimate(boolean b) {
+    animate = b;
+  }
+
+  public static void clearTerminal() {
+    System.out.println("\033[2J");
+  }
+
+  public static void gotoTop() {
+    System.out.println("\033[1;1H");
+  }
+
   
 }
