2b8d35085ba54d20bb5ed595fcbf900603be9690
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-01 14:33:34 -0500

    made mazegenerator file

---
+++ Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+  public static void main(String[] args) {
+    MazeGenerator1 a = new MazeGenerator1(50, 50, 600, 0);
+    
+  }
+}
---
+++ MazeGenerator.java
@@ -0,0 +1,5 @@
+public class MazeGenerator {
+  public static void generate(char[][] maze, int rows, int cols, int startrow, int startcol) {
+    
+  }
+}
