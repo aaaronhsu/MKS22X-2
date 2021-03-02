77a8a3341b4262c5f01ab9eee5a230bb185d3563
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-02 15:44:06 -0500

    end is placed at the farthest path

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -3,18 +3,17 @@ import java.io.*;
 
 public class MazeGenerator {
 
-  static ArrayList<int[]> empty = new ArrayList<>();
+  static int[] farthestSpace;
 
   public static void generate(char[][] maze, int rows, int cols, int startrow, int startcol) {
-    empty = new ArrayList<>();
-    generateMaze(maze, startrow, startcol);
-
+    farthestSpace = new int[] {0, startrow, startcol};
+    generateMaze(maze, startrow, startcol, 0);
+    
     maze[startrow][startcol] = 'S';
-    int[] endPos = empty.get((int) (Math.random() * empty.size()));
-    maze[endPos[0]][endPos[1]] = 'E';
+    maze[farthestSpace[1]][farthestSpace[2]] = 'E';
   }
 
-  private static void generateMaze(char[][] maze, int x, int y) {
+  private static void generateMaze(char[][] maze, int x, int y, int dist) {
     if (x <= 0 || x >= maze.length - 1 || y <= 0 || y >= maze[0].length - 1) return;
 
     int adjacentCarvedRegions = 0;
@@ -26,7 +25,10 @@ public class MazeGenerator {
     if (adjacentCarvedRegions >= 2) return;
 
     maze[x][y] = ' ';
-    empty.add(new int[] {x, y});
+    
+    if (dist > farthestSpace[0]) {
+      farthestSpace = new int[] {dist, x, y};
+    }
 
     ArrayList<Integer> order = new ArrayList<>();
     order.add(1);
@@ -36,10 +38,18 @@ public class MazeGenerator {
     Collections.shuffle(order);
     
     for (int i : order) {
-      if (i == 1) generateMaze(maze, x + 1, y);
-      else if (i == 2) generateMaze(maze, x - 1, y);
-      else if (i == 3) generateMaze(maze, x, y + 1);
-      else generateMaze(maze, x, y - 1);
+      if (i == 1) generateMaze(maze, x + 1, y, dist + 1);
+      else if (i == 2) generateMaze(maze, x - 1, y, dist + 1);
+      else if (i == 3) generateMaze(maze, x, y + 1, dist + 1);
+      else generateMaze(maze, x, y - 1, dist + 1);
+    }
+  }
+
+  static class Space implements Comparable<Space> {
+    int x, y, dist;
+
+    public int compareTo(Space other) {
+      return Integer.compare(this.dist, other.dist);
     }
   }
 
