74d48e4ad171e00031f8dd27a0a87b40bdfba84b
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-02 16:25:23 -0500

    changed randomization method to use steps instead of randomly generating at each step

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -48,13 +48,13 @@ public class MazeGenerator {
 
   public static void generateAmazing(char[][] maze, int row, int col, int startrow, int startcol) {
     farthestSpace = new int[] {0, startrow, startcol};
-    generateAmazingMaze(maze, startrow, startcol, 0, 0);
+    generateAmazingMaze(maze, startrow, startcol, 0, 0, 0);
 
     maze[startrow][startcol] = 'S';
     maze[farthestSpace[1]][farthestSpace[2]] = 'E';
   }
 
-  private static void generateAmazingMaze(char[][] maze, int x, int y, int dist, int pref) {
+  private static void generateAmazingMaze(char[][] maze, int x, int y, int dist, int dir, int steps) {
     if (x <= 0 || x >= maze.length - 1 || y <= 0 || y >= maze[0].length - 1) return;
 
     int adjacentCarvedRegions = 0;
@@ -78,16 +78,62 @@ public class MazeGenerator {
     order.add(4);
     Collections.shuffle(order);
 
-    if (pref != 0 && Math.random() < .9) {
-      order.remove((Integer) pref);
-      order.add(0, pref);
+    order.remove((Integer) dir);
+
+    if (dir != 0 && steps > 0) {
+      order.add(0, dir);
+      steps--;
     }
+    else if (steps == 0) steps = (int) (Math.random() * 10 + 5);
+
+    // some more randomization because why not
+    if (Math.random() < 0.5) order.remove(order.size() - 1);
     
     for (int i : order) {
-      if (i == 1) generateAmazingMaze(maze, x + 1, y, dist + 1, 1);
-      else if (i == 2) generateAmazingMaze(maze, x - 1, y, dist + 1, 2);
-      else if (i == 3) generateAmazingMaze(maze, x, y + 1, dist + 1, 3);
-      else generateAmazingMaze(maze, x, y - 1, dist + 1, 4);
+      if (i == 1) generateAmazingMaze(maze, x + 1, y, dist + 1, 1, steps);
+      else if (i == 2) generateAmazingMaze(maze, x - 1, y, dist + 1, 2, steps);
+      else if (i == 3) generateAmazingMaze(maze, x, y + 1, dist + 1, 3, steps);
+      else generateAmazingMaze(maze, x, y - 1, dist + 1, 4, steps);
     }
   }
+
+
+  // unfortunately this is way of generation doesn't create straight mazes all the time, just most of the time
+
+  // private static void generateAmazingMaze(char[][] maze, int x, int y, int dist, int pref) {
+  //   if (x <= 0 || x >= maze.length - 1 || y <= 0 || y >= maze[0].length - 1) return;
+
+  //   int adjacentCarvedRegions = 0;
+  //   if (maze[x + 1][y] != '#') adjacentCarvedRegions++;
+  //   if (maze[x - 1][y] != '#') adjacentCarvedRegions++;
+  //   if (maze[x][y + 1] != '#') adjacentCarvedRegions++;
+  //   if (maze[x][y - 1] != '#') adjacentCarvedRegions++;
+
+  //   if (adjacentCarvedRegions >= 2) return;
+
+  //   maze[x][y] = ' ';
+    
+  //   if (dist > farthestSpace[0]) {
+  //     farthestSpace = new int[] {dist, x, y};
+  //   }
+
+  //   ArrayList<Integer> order = new ArrayList<>();
+  //   order.add(1);
+  //   order.add(2);
+  //   order.add(3);
+  //   order.add(4);
+  //   Collections.shuffle(order);
+
+  //   if (pref != 0 && Math.random() < .9) {
+  //     order.remove((Integer) pref);
+  //     order.add(0, pref);
+  //   }
+    
+  //   for (int i : order) {
+  //     if (i == 1) generateAmazingMaze(maze, x + 1, y, dist + 1, 1);
+  //     else if (i == 2) generateAmazingMaze(maze, x - 1, y, dist + 1, 2);
+  //     else if (i == 3) generateAmazingMaze(maze, x, y + 1, dist + 1, 3);
+  //     else generateAmazingMaze(maze, x, y - 1, dist + 1, 4);
+  //   }
+  // }
 }
