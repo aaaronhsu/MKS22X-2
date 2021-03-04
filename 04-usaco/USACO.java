ce1fee56699c94e90f4cccbf0b30465eb677632a
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-04 16:40:10 -0500

    created input reading

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -1,10 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO {
   
   public static int bronze(String f) {
-
+    try {
+      Scanner in = new Scanner(new File(f));
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 
   public static long silver(String f) {
-    
+    try {
+      Scanner in = new Scanner(new File(f));
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 }
