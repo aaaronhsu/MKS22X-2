c1331a1b79190cc855471a0a59b235703903b4f4
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-04 16:47:37 -0500

    calculated aggregate depth

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -17,6 +17,35 @@ public class USACO {
         board[i][j] = in.nextInt();
       }
     }
+
+    for (int i = 0; i < n; i++) {
+      int a = in.nextInt() - 1;
+      int b = in.nextInt() - 1;
+      int stomps = in.nextInt();
+
+      int max = -1;
+      for (int c = a; c < a + 3; c++) {
+        for (int d = b; d < b + 3; d++) {
+          if (c >= x || d >= y) continue;
+          max = Integer.max(max, board[c][d]);
+        }
+      }
+
+      for (int c = a; c < a + 3; c++) {
+        for (int d = b; d < b + 3; d++) {
+          if (c >= x || d >= y) continue;
+          board[c][d] = max - stomps < board[c][d] ? max - stomps : board[c][d];
+        }
+      }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < x; i++) {
+      for (int j = 0; j < y; j++) {
+        ans += 0 < board[i][j] && board[i][j] < depth ? depth - board[i][j] : 0;
+      }
+    }
   }
 
   public static long silver(String f) throws FileNotFoundException {
