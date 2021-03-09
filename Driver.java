4a72014d89fc4d5822a6541dc51e1ce545419fe8
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-09 15:02:40 -0500

    tested function

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Driver {
   public static void main(String[] args) {
-    int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int[] arr = {1, 2, 2, 2, 2, 2, 2, 5};
     System.out.println(Preliminary.partition(arr, 0, arr.length - 1));
     System.out.println(Arrays.toString(arr));
   }
