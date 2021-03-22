b0a73437f70bc601ad3d083beaf8506fb32f5015
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-22 19:07:26 -0400

    constructor and tostring

--- MyDeque.java
+++ MyDeque.java
@@ -15,5 +15,30 @@ public class MyDeque<E> {
     size = 0; 
   }
 
-  
+  public int size() {
+    return size;
+  }
+
+  public String toString() {
+    StringBuilder str = new StringBuilder();
+    str.append("{");
+
+    if (start > 0) {
+      for (int i = start - 1; i > 0; i--) str.append(data[i] + ", ");
+      if (end == data.length - 1) {
+        str.append(data[0] + "}");
+        return str.toString();
+      }
+      else str.append(data[0]);
+    }
+
+    if (end < data.length - 1) {
+      for (int i = data.length - 1; end + 1 < i; i--) str.append(data[i] + ", ");
+      str.append(data[end + 1] + "}");
+      return str.toString();
+    }
+
+    str.append("}");
+    return str.toString();
+  }
 }
