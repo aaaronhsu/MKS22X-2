ea3016742103f8844cb1a33d8241942007275b18
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-22 00:40:42 -0400

    size

--- MyDeque.java
+++ MyDeque.java
@@ -5,12 +5,21 @@ public class MyDeque<E> {
   @SuppressWarnings("unchecked")
   public MyDeque() {
     data = (E[]) new Object[10];
+    start = 0;
+    end = 0;
+    size = 0; 
   }
 
   @SuppressWarnings("unchecked")
   public MyDeque(int n) {
     data = (E[]) new Object[n];
+    start = 0;
+    end = 0;
+    size = 0; 
   }
 
-  
+  public int size() {
+    if (end > start) return end - start + 1;
+    return end + (data.length - start) + 1;
+  }
 }
