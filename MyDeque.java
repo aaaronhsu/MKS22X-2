326b6bcbeeb87775bd906b4c09a36ab4b92df373
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-22 00:49:01 -0400

    toString

--- MyDeque.java
+++ MyDeque.java
@@ -22,4 +22,30 @@ public class MyDeque<E> {
     if (end > start) return end - start - 1;
     return end + (data.length - start);
   }
+
+  public String toString() {
+    StringBuilder str = new StringBuilder();
+    str.append("{");
+
+    if (end > start) {
+      for (int i = start + 1; i < end - 1; i++) str.append(data[i] + ", ");
+      str.append(data[end - 1] + "}");
+    }
+    else {
+      for (int i = start + 1; i < data.length - 1; i++) str.append(data[i] + ", ");
+      if (end != 0) {
+        str.append(data[data.length - 1] + ", ");
+      }
+      else {
+        str.append(data[data.length - 1] + "}");
+        return str.toString();
+      }
+
+      for (int i = 0; i < end - 1; i++) str.append(data[i] + ", ");
+      
+      str.append(data[end - 1] + "}");
+    }
+
+    return str.toString();
+  }
 }
