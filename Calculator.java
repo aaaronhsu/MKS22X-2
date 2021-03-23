910f47e574bc3ec09a5b8a38dccceece9539ec99
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-23 14:06:32 -0400

    calculator file created

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.util.ArrayDeque;
+
+public class Calculator {
+  public static double eval(String s) {
+    if (s.length() == 0) throw new IllegalArgumentException();
+
+    ArrayDeque<Double> stack = new ArrayDeque<>();
+
+  }
+}
