76d9564bd95e1138f6b58a33e11d944cefb1fe02
Aaron Hsu <aaronhsu4606@gmail.com>
2021-04-02 12:16:16 -0400

    file organization

--- 08-calculator/Driver.java
+++ 08-calculator/Driver.java
@@ -3,7 +3,7 @@ import java.util.ArrayDeque;
 public class Driver {
   public static void main(String[] args) {
     
-    String str = "3 2 %";
+    String str = "1 2 3 4 5 + * - -";
     System.out.println(Calculator.eval(str));
   }
 }
