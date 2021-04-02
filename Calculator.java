69c61311beffcc33e341ec12ff14c448491d2355
Aaron Hsu <aaronhsu4606@gmail.com>
2021-04-02 12:06:19 -0400

    added calculator parsing & argument exception

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,31 @@
 import java.util.ArrayDeque;
 
+import java.util.*;
+import java.io.*;
+
 public class Calculator {
   public static double eval(String s) {
     if (s.length() == 0) throw new IllegalArgumentException();
 
     ArrayDeque<Double> stack = new ArrayDeque<>();
+    
+    Scanner in = new Scanner(s);
 
+    while (in.hasNext()) {
+      String str = in.next();
+
+      try {
+        stack.add(Double.parseDouble(str));
+        System.out.println("added " + Double.parseDouble(str));
+      }
+      catch (NumberFormatException e) {
+        if (stack.size() <= 1) throw new IllegalArgumentException("too many operands");
+        System.out.println("not a number");
+      }
+    }
+
+    if (stack.size() != 1) throw new IllegalArgumentException("too few operands");
+
+    return stack.poll();
   }
 }
---
+++ Driver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayDeque;
+
+public class Driver {
+  public static void main(String[] args) {
+    
+
+    Calculator.eval("+");
+  }
+}
